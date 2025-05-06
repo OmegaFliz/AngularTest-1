f6433e6b8037a0d91a27f9c7d4e7550b3b416bef
OmegaFliz <linkhant873@gmail.com>
2025-05-07 01:13:09 +0630

    hello component added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HelloComponent } from "./hello/hello.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,CommonModule],
+  imports: [RouterOutlet, CommonModule, HelloComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/hello/hello.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-hello',
+  imports: [],
+  templateUrl: './hello.component.html',
+  styleUrl: './hello.component.css'
+})
+export class HelloComponent {
+
+  message = "Hello this is the message from hello component TS!"
+
+}
